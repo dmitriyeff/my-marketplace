d98569b9c84ce9e6044c76b7bc775e56d9f1bfdc
Denis <deniss.dmitrijevs@pipedrive.com>
2019-12-24 23:21:31 +0200

    create separate component for mapping cards and using Container and CardDeck

---
+++ src/components/CardsContainer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Cards from "../components/cards/Card";
+import jsonData from "../assets/apps";
+import Container from "react-bootstrap/Container";
+import CardDeck from "react-bootstrap/CardDeck";
+
+import AppContext from "../context/app-context";
+
+const CardsContainer = () => {
+    return (
+        <Container>
+            <CardDeck>
+                {jsonData.map(app => (
+                    <AppContext.Provider
+                        key={app.id}
+                        value={{
+                            title: app.title,
+                            description: app.description,
+                            image: app.image
+                        }}
+                    >
+                        <Cards />
+                    </AppContext.Provider>
+                ))}
+            </CardDeck>
+        </Container>
+    );
+};
+
+export default CardsContainer;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,34 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import HeaderContainer from "../components/HeaderContainer";
-import Cards from "../../src/components/cards/Card";
-
-import CardDeck from "react-bootstrap/CardDeck";
-import Container from "react-bootstrap/Container";
-import jsonData from "../assets/apps.json";
-
-import AppContext from "../context/app-context";
+import CardsContainer from "../components/CardsContainer";
 
 class App extends Component {
     render() {
         return (
             <Fragment>
                 <HeaderContainer />
-                <Container>
-                    <CardDeck>
-                    {jsonData.map(app => (
-                        <AppContext.Provider
-                            key={app.id}
-                            value={{
-                                title: app.title,
-                                description: app.description,
-                                image: app.image
-                            }}
-                        >
-                            <Cards />
-                        </AppContext.Provider>
-                    ))}
-                    </CardDeck>
-                </Container>
+                <CardsContainer />
             </Fragment>
         );
     }
