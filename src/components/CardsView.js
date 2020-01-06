ccccd4656788385771ff127aa71b01cd9a702cbb
Denis <deniss.dmitrijevs@pipedrive.com>
2020-01-06 09:01:02 +0200

    remove switch because it is not needed

--- src/components/CardsView.js
+++ src/components/CardsView.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import CardDeck from "react-bootstrap/CardDeck";
 import AppsList from "./apps/AppsList";
 import AppDetails from "./apps/AppDetails";
 import {
     BrowserRouter as Router,
-    Switch,
     Route,
     Redirect
 } from "react-router-dom";
@@ -15,13 +13,11 @@ const CardsView = () => {
     return (
         <Container>
             <Router>
-                <Switch>
-                    <Route exact path="/" component={AppsList}>
-                        <Redirect from="/" to="/apps"/>
-                    </Route>
-                    <Route exact path="/apps" component={AppsList} />
-                    <Route path="/apps/:id" component={AppDetails} />
-                </Switch>
+                <Route exact path="/" component={AppsList}>
+                    <Redirect from="/" to="/apps"/>
+                </Route>
+                <Route exact path="/apps" component={AppsList} />
+                <Route path="/apps/:id" component={AppDetails} />
             </Router>
         </Container>
     );
