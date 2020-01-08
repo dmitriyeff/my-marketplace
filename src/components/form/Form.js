ebe406efa3232d10e8cefc02df0238bcf135112d
Denis <deniss.dmitrijevs@pipedrive.com>
2020-01-08 23:39:34 +0200

    implement validation

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,60 +1,62 @@
-import React, { Fragment } from "react";
-import { Form, Col, Row, Button, ButtonToolbar } from "react-bootstrap";
+import React, { Fragment, useState } from "react";
+import { Form, Col, Row, Button, ButtonToolbar, InputGroup } from "react-bootstrap";
 import GoBackButton from "../buttons/GoBack";
 
 import "./styles.scss";
 
 const AppForm = () => {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = event => {
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
     return (
         <Fragment>
-            <Form className="form-st" as={Col} md="8">
-                <Form.Row>
-                    <Form.Group as={Col} md="4" controlId="validationCustom01">
-                        <Form.Label>Your app name</Form.Label>
-                        <Form.Control
-                            required
-                            type="text"
-                            placeholder="Example app"
-                        />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                    <Form.Group as={Col} md="8" controlId="validationCustom02">
-                        <Form.Label>App description</Form.Label>
-                        <Form.Control
-                            as="textarea" rows="3"
-                            required
-                            type="text"
-                            placeholder="Describe your application"
-                        />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-
-                    </Form.Group>
-                </Form.Row>
-            </Form>
-            <Row
-                as={Col}
-                md="8"
-                style={{
-                    margin: "auto",
-                    padding: "0"
-                }}>
-                <ButtonToolbar
-                    style={{
-                        width: "25%",
-                        display: "flex",
-                        justifyContent: "space-between"}}
+            <div>
+                <Form className="form-st"
+                      noValidate
+                      validated={validated}
+                      onSubmit={handleSubmit}
                 >
-                    <GoBackButton />
-                    <Button
-                        variant="outline-danger"
-                        type="submit"
+                    <Form.Row>
+                        <Form.Group as={Col} md="4" controlId="validationCustom01">
+                            <Form.Label>App name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Name your app"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} md="4" controlId="validationCustom02">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                as="textarea"
+                                placeholder="Add app description"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Form.Row>
+                    <Button type="submit"
+                            variant="outline-primary"
                     >
-                        Submit
+                        Submit form
                     </Button>
-                </ButtonToolbar>
-            </Row>
+                </Form>
+                <GoBackButton />
+            </div>
         </Fragment>
     );
 };
